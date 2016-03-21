19f11ae3739f6c5a8dd54a65b29d71459204b7fe
Erika Heidi <erika.christina@gmail.com>
2016-03-21 19:05:37 +0100

    adding puppet

---
+++ puppet/environments/vagrant/manifests/default.pp
@@ -0,0 +1,40 @@
+$doc_root = "/var/www/example"
+
+#executes apt-get update
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update'
+}
+
+#install Apache
+package { 'apache2':
+  ensure  => "installed",
+  require => Exec['apt-get update']
+}
+
+#create doc root directory
+file { $doc_root:
+  ensure => "directory",
+  owner => "www-data",
+  group => "www-data",
+  mode => "644"
+}
+
+#copy index html file
+file { "$doc_root/index.html":
+    ensure => "present",
+    source => "puppet:///modules/main/index.html"
+}
+
+#set vhost template
+file { "/etc/apache2/sites-available/000-default.conf":
+    ensure => "present",
+    content => template("main/vhost.erb"),
+    notify => Service['apache2'],
+    require => Package['apache2']
+}
+
+#define the Apache service
+service { 'apache2':
+    ensure => running,
+    enable => true
+}
